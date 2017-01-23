ba058cd9da7c6ffac58c017685402f9aae02e006
SapNeg <sapna.negi13@gmail.com>
2017-01-23 17:34:57 +0000

    Add files via upload

--- suggestionAPI.py
+++ suggestionAPI.py
@@ -1,46 +1,25 @@
 from __future__ import print_function
 from flask import Flask, jsonify, request
-import operator
 import numpy as np
-import sys
-
-np.random.seed(1337)  # for reproducibility
-
 import os
 import codecs
 import csv
 import re
+import nltk
 from sklearn.feature_extraction.text import CountVectorizer
+os.environ['KERAS_BACKEND']='theano'
 from keras.preprocessing import sequence
 from keras.models import load_model
-from keras.utils.np_utils import to_categorical
-from sklearn.metrics import f1_score
-from keras.preprocessing.text import Tokenizer
-from keras.models import Sequential
-from keras.layers import Embedding
-from keras import backend as K
 app = Flask(__name__)
 
 
 # set parameters:
-testPath = "/Users/sapna/PycharmProjects/MEsentiment/data/suggestion/tweets_microsoft.csv"
-#savedModelPath = http://server1.nlp.insight-centre.org/MEsavedModels/CNN_twitterSugg_model.h5
-Embedding_dir = "/Users/sapna/PycharmProjects/Keyphrase-Extractor/Glove/glove.twitter.27B.50d.txt"
-savedModelPath = "/Users/sapna/PycharmProjects/MEsentiment/savedModels/CNN_twitterSugg_model.h5"
+Root_dir = os.path.dirname(os.path.abspath(__file__))
+Embedding_dir = Root_dir + "/Embeddings/glove.twitter.27B.50d.txt"
+savedModelPath = Root_dir + "/savedModels/CNN_twitterSugg_model.h5"
 max_no_words = 27000000000 #fixed, should be same as the number of words in embeddings
 maxlen= 40
-text_instance = "some tweet"
-
-
-# set parameters:
-testPath = "/Users/sapna/PycharmProjects/MEsentiment/data/suggestion/tweets_microsoft.csv"
-#savedModelPath = http://server1.nlp.insight-centre.org/MEsavedModels/CNN_twitterSugg_model.h5
-Embedding_dir = "/Users/sapna/PycharmProjects/Keyphrase-Extractor/Glove/glove.twitter.27B.50d.txt"
-savedModelPath = "/Users/sapna/PycharmProjects/MEsentiment/savedModels/CNN_twitterSugg_model.h5"
-max_no_words = 27000000000 #fixed, should be same as the number of words in the pretrained embeddings
-maxlen= 40
-text_instance = "some tweet"
-
+np.random.seed(1337)  # for reproducibility
 
 # start process_tweet
 def cleanTweet(tweet):
@@ -125,21 +104,20 @@ def classify(text):
     y_test_predict = classifierModel.predict_classes(X_test)
 
     if(y_test_predict==1):
-            print("positive")
+            return("suggestion")
     else:
-            print("negative")
+            return("non-suggestion")
 
 def split_sentences(text):
     """
     Utility function to return a list of sentences.
     @param text The text that must be split in to sentences.
     """
-    sentence_delimiters = re.compile(u'[.!?,;:\t\\\\"\\(\\)\\\'\u2019\u2013]|\\s\\-\\s')
-    sentences = sentence_delimiters.split(text)
+    tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+    sentences = tokenizer.tokenize(text)
     return sentences
 
-
-
+@app.route('/<text>', methods = ['GET'])
 def run(text):
     sentence_list = split_sentences(cleanTweet(text))
     label = "non-suggestion"
@@ -150,12 +128,5 @@ def run(text):
 
     return label
 
-
-@app.route('/<inputSentence>', methods = ['GET'])
-def test(inputSentence):
-    text = inputSentence
-    sentenceList = split_sentences(text)
-    return run(text)
-
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug = True, host='0.0.0.0')
